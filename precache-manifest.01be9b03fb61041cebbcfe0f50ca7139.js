f575bd73c22bcbe16761109922022b1737397c37
kishibe-rohan <shahnawaz.21810589@viit.ac.in>
2020-09-14 22:26:56 +0530

    Updates

---
+++ precache-manifest.01be9b03fb61041cebbcfe0f50ca7139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9ed7916298e18513132",
+    "url": "/react-recipe-app/static/js/runtime~main.f6d3a2cf.js"
+  },
+  {
+    "revision": "7d5be0bd9472ab18f474",
+    "url": "/react-recipe-app/static/js/main.c677f41e.chunk.js"
+  },
+  {
+    "revision": "00ec2785f98d8cbe3dbf",
+    "url": "/react-recipe-app/static/js/2.aab98692.chunk.js"
+  },
+  {
+    "revision": "7d5be0bd9472ab18f474",
+    "url": "/react-recipe-app/static/css/main.5f2b201b.chunk.css"
+  },
+  {
+    "revision": "19c59dfa0a935a131b832f3ce4164f88",
+    "url": "/react-recipe-app/index.html"
+  }
+];
